056fb09c7b7d10da7563e47c62470fd8907b7400
WilliamSado <williamsado1004@gmail.com>
2026-07-26 22:01:27 +0800

    extra: Add face unlock and optional GMS configuration

--- BoardConfigExtra.mk
+++ BoardConfigExtra.mk
@@ -7,8 +7,3 @@ EXTRA_PATH := vendor/extra
 # FaceUnlock
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(EXTRA_PATH)/facehal/default/compatibility_matrix.xml
-
-# GMS
-ifeq ($(WITH_GMS), true)
-$(call inherit-product-if-exists, vendor/gms/gms_full.mk)
-endif
--- product.mk
+++ product.mk
@@ -1,2 +1,7 @@
 # FaceUnlock
 $(call inherit-product, vendor/extra/facehal/facehal.mk)
+
+# GMS
+ifeq ($(WITH_GMS),true)
+$(call inherit-product-if-exists, vendor/gms/gms_full.mk)
+endif
